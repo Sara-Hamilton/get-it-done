9966f82e50a98ea85c426f3d95a83cbef9534523
Sara Hamilton <Sara-Hamilton@users.noreply.github.com>
2017-07-10 10:42:16 -0700

    completed class 2.10 prep

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, session
+from flask import Flask, request, redirect, render_template, session, flash
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -15,16 +15,19 @@ class Task(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
     completed = db.Column(db.Boolean)
+    owner_id = db.Column(db.Integer, db.ForeignKey('user.id'))
 
-    def __init__(self, name):
+    def __init__(self, name, owner):
         self.name = name
         self.completed = False
+        self.owner = owner
 
 class User(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True)
     password = db.Column(db.String(120))
+    tasks = db.relationship('Task', backref='owner')
 
     def __init__(self, email, password):
         self.email = email
@@ -44,10 +47,10 @@ def login():
         user = User.query.filter_by(email=email).first()
         if user and user.password == password:
             session['email'] = email
+            flash("Logged in")
             return redirect('/')
         else:
-            # TODO - explain why login failed
-            return '<h1>Error!</h1>'
+            flash('User password incorrect, or user does not exist', 'error')
 
     return render_template('login.html')
 
@@ -82,14 +85,16 @@ def logout():
 @app.route('/', methods=["POST", "GET"])
 def index():
 
+    owner = User.query.filter_by(email=session['email']).first()
+
     if request.method == "POST":
         task_name = request.form['task']
-        new_task = Task(task_name)
+        new_task = Task(task_name, owner)
         db.session.add(new_task)
         db.session.commit()
 
-    tasks = Task.query.filter_by(completed = False).all()
-    completed_tasks = Task.query.filter_by(completed = True).all()
+    tasks = Task.query.filter_by(completed = False, owner=owner).all()
+    completed_tasks = Task.query.filter_by(completed = True, owner=owner).all()
     return render_template('todos.html', title="Get It Done!", 
         tasks=tasks, completed_tasks = completed_tasks)
 
